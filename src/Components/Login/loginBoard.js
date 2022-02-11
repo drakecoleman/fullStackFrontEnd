1989b00f06656666c3bbc5175fe202570a476d1a
Drake <apple@apples-MacBook-Pro.local>
2022-02-11 02:20:55 -0500

    changed http to https in login board so this thing will work

--- src/Components/Login/loginBoard.js
+++ src/Components/Login/loginBoard.js
@@ -16,7 +16,7 @@ function RegisterBoard() {
   }
   function submission(e) {
     e.preventDefault();
-    fetch("http://profilestiebackend.herokuapp.com/login", {
+    fetch("https://profilestiebackend.herokuapp.com/login", {
       method: "POST",
       credentials: "include",
       withCredentials: true,
